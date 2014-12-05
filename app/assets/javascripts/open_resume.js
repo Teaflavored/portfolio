c9f4454557967ca9339b8f360e6c2804ac49a14c
Auster Chen <Auster.s.chen@gmail.com>
2014-12-05 13:42:37 -0800

    finished sidebar content

--- app/assets/javascripts/open_resume.js
+++ app/assets/javascripts/open_resume.js
@@ -1,8 +1,9 @@
 $(function(){
   $("button.btn.view-resume").magnificPopup({
     items: {
-      src: 'https://docs.google.com/document/d/1ZTsQFccQLCIUsYcnF2sQsNP6sWuhdvTQ4mlmHJKTSqc',
+      src: 'https://drive.google.com/file/d/0B6oKzMNIDqDsLVZiN2pVaVA1MG8/view?usp=sharing',
       type: 'iframe' // this overrides default type
     },
+
   })
 })
