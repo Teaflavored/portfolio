eec236bc425131f43a0d1de1e8c0502f55b58028
Auster Chen <Auster.s.chen@gmail.com>
2014-12-24 14:45:21 -0800

    added portfolio window

---
+++ app/assets/javascripts/portfolio_link.js
@@ -0,0 +1,52 @@
+$(function(){
+  var $portfolioLink = $("a.portfolio-link");
+  var $portfolioContent = $("div#portfolio-content");
+  var $mainContent = $("div#center-pane");
+
+  var linkDoNothing = function(e){
+    e.preventDefault();
+    return false;
+  };
+
+  var openPortfolio = function(e){
+    if (!$portfolioContent.hasClass("opened")){
+      //expand portfolio area
+      $portfolioContent.css("width", "100%");
+      //add class opened
+      $portfolioContent.addClass("opened");
+      //addblur
+      $mainContent.addClass("blur");
+
+      //disabled link
+      $portfolioLink.unbind();
+      $portfolioLink.on("click", linkDoNothing);
+
+      $portfolioContent.on("transitionend", function(){
+        //re-enable link
+        $portfolioLink.unbind();
+        $portfolioLink.on("click", openPortfolio);
+      })
+
+
+    } else {
+      $portfolioContent.css("width", "0%");
+      $portfolioContent.removeClass("opened");
+
+      $portfolioLink.unbind();
+      $portfolioLink.on("click", linkDoNothing);
+
+
+      $portfolioContent.one("transitionend", function(){
+        $mainContent.removeClass("blur");
+
+        $portfolioLink.unbind();
+        $portfolioLink.on("click", openPortfolio);
+      });
+
+    }
+  }
+
+
+
+  $portfolioLink.on("click", openPortfolio);
+})
