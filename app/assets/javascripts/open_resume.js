da2d29453dd5f190674c3702038aa37c2ee3b61c
Auster Chen <Auster.s.chen@gmail.com>
2014-12-24 13:44:19 -0800

    fixed google docs iframe xframe same origin with embed link

--- app/assets/javascripts/open_resume.js
+++ app/assets/javascripts/open_resume.js
@@ -1,7 +1,7 @@
 $(function(){
   $("button.btn.view-resume").magnificPopup({
     items: {
-      src: 'https://drive.google.com/file/d/0B6oKzMNIDqDsLVZiN2pVaVA1MG8/view?usp=sharing',
+      src: 'https://drive.google.com/file/d/0B6oKzMNIDqDsLVZiN2pVaVA1MG8/preview',
       type: 'iframe' // this overrides default type
     },
 
