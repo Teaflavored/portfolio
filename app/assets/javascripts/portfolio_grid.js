1a336784fc5e51ae223cab72c2e6583e918ee8fd
Auster Chen <Auster.s.chen@gmail.com>
2014-12-24 19:03:39 -0800

    got tiles to expand horizontally

--- app/assets/javascripts/portfolio_grid.js
+++ app/assets/javascripts/portfolio_grid.js
@@ -4,7 +4,25 @@ $(function(){
   $items.gridList("resize", 3);
 
   $(window).resize(function(){
-    
+
     $items.gridList("resize", 3);
+  })
+
+
+  $items.find(".items").on("click", function(e){
+    var $clickedBox = $(e.currentTarget);
+    var $nonClickedBoxes = $clickedBox.siblings();
+
+    $clickedBox.addClass("active");
+    $nonClickedBoxes.removeClass("active");
+
+    $items.find(".items").each(function(listEl){
+      if ($(this).hasClass("active")){
+        $items.gridList("resizeItem", $(this), 3, 4);
+      } else {
+        $items.gridList("resizeItem", $(this), 1);
+      }
+    })
+
   })
 })
